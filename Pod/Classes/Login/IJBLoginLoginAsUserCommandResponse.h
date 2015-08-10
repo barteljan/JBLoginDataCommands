367f4fb35ad7780fc83ac1e45ea6b2c78c38bb96
Jan Bartel <jan.bartel@atino.de>
2015-08-10 12:59:12 +0200

    created new pod project

---
+++ Pod/Classes/Login/IJBLoginLoginAsUserCommandResponse.h
@@ -0,0 +1,13 @@
+//
+//  ILoginUserCommandResponse.h
+//  Pods
+//
+//  Created by bartel on 10.08.15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol IJBLoginLoginAsUserCommandResponse <NSObject>
+-(NSString*)token;
+@end
